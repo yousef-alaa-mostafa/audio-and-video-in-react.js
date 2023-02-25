1c49d48f4eec958c036eada01123a60abc8c3356
yousef-alaa-mostafa <yousefalaa67@gmail.com>
2023-02-25 22:03:41 +0200

    update

---
+++ audio-and-video-in-react/src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+
+import style_ from "./style.module.css";
+
+import { ReactMediaRecorder } from "react-media-recorder";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Video recorder</h1>
+      <ReactMediaRecorder
+        video
+        render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+          <div>
+            <p className={style_.status}>{status}</p>
+            {/* status = ideal or acquiring_media or recording or stopped */}
+            {console.log(status)}
+            <video src={mediaBlobUrl} controls autoPlay loop />
+            <div>
+              <button className={style_.Btn} onClick={startRecording}>
+                Start Recording
+              </button>
+              <button className={style_.Btn} onClick={stopRecording}>
+                Stop Recording
+              </button>
+            </div>
+          </div>
+        )}
+      />
+    </div>
+  );
+}
+
+export default App;
